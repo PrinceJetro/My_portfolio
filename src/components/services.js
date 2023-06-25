3b6aae3d9e7b5b639b5f44d60666e4e1fff96e5b
PrinceJetro <adegbuyijephthah@gmail.com>
2023-06-25 12:49:06 +0100

    Added info

--- src/components/services.js
+++ src/components/services.js
@@ -9,6 +9,7 @@ export default function App(){
         <div className='serviceDiv'>
            <h1>Services</h1>
            <hr/>
+           <p>Click on the images to see  their entailments</p>
            <div className='d-flex flex-wrap'>
            <Service title="Web Development" description="I can create responsive and functional websites using various technologies such as HTML, CSS, JavaScript, and frameworks like React. I ensure the websites I build are user-friendly, visually appealing, and optimized for performance." img="https://media.publit.io/file/w_1920,h_864,c_fit,q_80/Blogweb/2s_blog_04_web-dev-maintain.jpg"/>
            <Service title="Backend Development" description="I have experience in building server-side applications and APIs using languages like Python, Django, or Flask. I can design and implement robust and scalable backend systems to support web applications." img="https://www.hubsol.com/public/upload/post/91261_web-development-lahore.jpg"/>
